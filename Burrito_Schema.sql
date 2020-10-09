b9936336f60830e83b4ea454b7126e91a4c3a5fc
Stephen Hong <hongsulbum@gmail.com>
2020-10-08 19:44:59 -0700

    trying to change to branch

--- Burrito_Schema.sql
+++ Burrito_Schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE SD_Burrito_Data;
 
-copy SD_Burrito_Data(Location, Burrito, Neighborhood, Address, Yelp, Google, Cost)
-from 'C:\Users\Kristi\Documents\Final_Burrito.csv'
+copy SD_Burrito_Data(Location, Burrito, Neighborhood, Address, Yelp, Google, Cost, key)
+from 'C:\Users\shong3\data_project\San-Diego-Burrito-Project\Final_Burrito.csv'
 delimiter ','
 CSV HEADER;
 
